1d4af7eaedc79cdf19effb20f6b25a90068cfc6a
Andrei Mihaescu <amihaescu@deloittece.com>
2018-06-21 18:56:36 +0300

    Added a basic controller

---
+++ src/main/java/com/deloitte/digital/erdc/gitintro/controller/NamesController.java
@@ -0,0 +1,19 @@
+package com.deloitte.digital.erdc.gitintro.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/name")
+public class NamesController {
+
+    @RequestMapping
+    public List<String> getNames(){
+        List<String> names = new ArrayList<>();
+        return names;
+    }
+
+
+}
