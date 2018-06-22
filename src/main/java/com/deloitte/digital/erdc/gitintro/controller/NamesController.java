3086940efceed6ec1e464609027464df0be0e22a
Calistru <icalistru@roicalistru01m.atrema.deloitte.com>
2018-06-22 12:47:01 +0300

    Added my name to the List

--- src/main/java/com/deloitte/digital/erdc/gitintro/controller/NamesController.java
+++ src/main/java/com/deloitte/digital/erdc/gitintro/controller/NamesController.java
@@ -13,6 +13,7 @@ public class NamesController {
     public List<String> getNames(){
         List<String> names = new ArrayList<>();
         names.add("Default");
+        names.add("Alex");
         return names;
     }
 
